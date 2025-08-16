4bd38dbcf5408e72b18febb29176c7dd11df4b57
MrVain1979 <MrVain1979@users.noreply.github.com>
2025-08-16 20:28:57 +0200

    web: remove Pages Router API routes

--- apps/web/pages/api/presentation-draft.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  // Aktiver preview-mode
-  res.setPreviewData({});
-  res.writeHead(307, { Location: "/" });
-  res.end();
-}
