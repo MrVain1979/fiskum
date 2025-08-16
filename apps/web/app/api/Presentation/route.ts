217ca46abf0b084ff50e219da711113c5da3f991
MrVain1979 <MrVain1979@users.noreply.github.com>
2025-08-16 20:36:06 +0200

    web: add App Router presentation endpoints

---
+++ apps/web/app/api/Presentation/route.ts
@@ -0,0 +1,8 @@
+﻿import { draftMode } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  draftMode().enable();
+  const url = new URL("/", req.nextUrl);
+  return NextResponse.redirect(url);
+}
